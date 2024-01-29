4ab7ee4e3b7253ad874c4d9648a5c601418b1c23
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-29 16:53:42 -0300

    Esqueleto do back-end

---
+++ Back_end/src/main/java/com/navarro/userRegistration/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.navarro.userRegistration.repository;
+
+import com.navarro.userRegistration.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
